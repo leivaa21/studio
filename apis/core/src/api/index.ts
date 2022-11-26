c183d65ba8f93ec46df5f91715b8f6d6f764efa8
leivaa <adrianleiva212@gmail.com>
2022-11-26 18:52:38 +0100

    feat: started core api

---
+++ apis/core/src/api/index.ts
@@ -0,0 +1,18 @@
+import { createExpressServer } from 'routing-controllers';
+import 'reflect-metadata';
+import { info } from '@studio/api-utils/loggers/console';
+import { EarlyMiddlewares, LateMiddlewares } from './middlewares';
+import { Controllers } from './controllers';
+
+const port = 3000;
+
+const { NODE_ENV } = process.env;
+
+createExpressServer({
+  controllers: [...Controllers],
+  middlewares: [...EarlyMiddlewares, ...LateMiddlewares],
+  interceptors: [],
+  defaultErrorHandler: false,
+}).listen(port, () => {
+  info(`Hey, running on http://localhost:${port} on env: ${NODE_ENV}`);
+});
---
+++ apis/core/src/api/middlewares/index.ts
@@ -0,0 +1,9 @@
+import { CustomErrorHandler } from "./after/ErrorHandler";
+import { RouteNotFoundMiddleware } from "./after/RouteNotFound";
+import { RequestConsoleLoggerMiddleware } from "./before/RequestConsoleLogger";
+
+const EarlyMiddlewares = [RequestConsoleLoggerMiddleware]
+const LateMiddlewares = [CustomErrorHandler, RouteNotFoundMiddleware]
+
+
+export {EarlyMiddlewares, LateMiddlewares}
