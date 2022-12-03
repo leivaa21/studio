d51be5f3590c809a5ec1874267ea8c0f390e0d54
leivaa <adrianleiva212@gmail.com>
2022-12-03 18:14:22 +0100

    feat: example Login Form with story

---
+++ apps/storybook/src/stories/components/Form.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "@studio/ui/components/interactivity/cta/button";
+
+import Form, {FormBody, FormTextInput, FormHeader, FormHint} from '@studio/ui/components/interactivity/form';
+import React from "react";
+
+export default {
+  title: 'Components/Form',
+  component: Form,
+} as ComponentMeta<typeof Form>;
+
+
+const ExampleLoginForm: ComponentStory<typeof Form> = (args) => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+
+  return (
+    <Form style={{ width: '450px' }}>
+      <FormHeader>
+        <h1><b>Join now</b> and get access to <b>all</b> the courses!🚀</h1>
+      </FormHeader>
+      <FormBody>
+        <FormTextInput
+          Name="Email"
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <FormTextInput
+          Name="Password"
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <Button Label="sign in!" Size="Medium" Type="Primary" />
+        <FormHint>
+          <p>You dont have account yet?</p>
+          <a style={{ cursor: 'pointer' }}>Sing up here!</a>
+        </FormHint>
+      </FormBody>
+    </Form>
+  )
+};
+
+export const LoginForm = ExampleLoginForm.bind({});
