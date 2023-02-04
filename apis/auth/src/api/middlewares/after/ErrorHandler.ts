3aace3a1d286debc425a3e0b22919ca7c886b48b
leivaa <adrianleiva212@gmail.com>
2023-02-04 14:50:56 +0100

    chore: linted all code

--- apis/auth/src/api/middlewares/after/ErrorHandler.ts
+++ apis/auth/src/api/middlewares/after/ErrorHandler.ts
@@ -1,15 +1,18 @@
 import { ErrorHandler } from '@studio/api-utils/middlewares';
 import { NextFunction, Request, Response } from 'express';
-import { Middleware, ExpressErrorMiddlewareInterface } from 'routing-controllers';
+import {
+  Middleware,
+  ExpressErrorMiddlewareInterface,
+} from 'routing-controllers';
 
 @Middleware({ priority: 0, type: 'after' })
 export class CustomErrorHandler implements ExpressErrorMiddlewareInterface {
-  error(error: Error, request: Request, response: Response, next: NextFunction) {
-    ErrorHandler(
-      error,
-      request,
-      response,
-      next
-    );
+  error(
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    ErrorHandler(error, request, response, next);
   }
-} 
+}
--- apis/auth/src/api/middlewares/before/RequestConsoleLogger.ts
+++ apis/auth/src/api/middlewares/before/RequestConsoleLogger.ts
@@ -2,13 +2,11 @@ import { RequestConsoleLogger } from '@studio/api-utils/middlewares';
 import { NextFunction, Request, Response } from 'express';
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
 
-@Middleware({type: 'before'})
-export class RequestConsoleLoggerMiddleware implements ExpressMiddlewareInterface {
+@Middleware({ type: 'before' })
+export class RequestConsoleLoggerMiddleware
+  implements ExpressMiddlewareInterface
+{
   use(request: Request, response: Response, next: NextFunction) {
-    RequestConsoleLogger(
-      request,
-      response,
-      next
-    );
+    RequestConsoleLogger(request, response, next);
   }
-} 
+}
