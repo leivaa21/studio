d3a98a5789420fab51639c9d6bbcdb4a82efd3fb
leivaa <adrianleiva212@gmail.com>
2023-02-21 16:54:05 +0100

    style: linted users module

--- apis/auth/src/contexts/users/domain/UserNickname.ts
+++ apis/auth/src/contexts/users/domain/UserNickname.ts
@@ -1,15 +1,15 @@
-import { ValueObject } from "../../shared/domain/valueObjects/ValueObject";
-import UserNicknameLenghtExceeded from "./exceptions/UserNicknameLengthExceeded";
+import { ValueObject } from '../../shared/domain/valueObjects/ValueObject';
+import UserNicknameLenghtExceeded from './exceptions/UserNicknameLengthExceeded';
 
 class UserNickname extends ValueObject<string> {
-  static MAX_LENGTH = 16; 
+  static MAX_LENGTH = 16;
   constructor(value: string) {
     super(value);
     this.assertLength();
   }
 
   assertLength(): void {
-    if(this.value.length > UserNickname.MAX_LENGTH) {
+    if (this.value.length > UserNickname.MAX_LENGTH) {
       throw new UserNicknameLenghtExceeded(this.value);
     }
   }
--- apis/auth/src/contexts/users/domain/exceptions/UserNicknameLengthExceeded.ts
+++ apis/auth/src/contexts/users/domain/exceptions/UserNicknameLengthExceeded.ts
@@ -1,10 +1,15 @@
-import { Exception } from "../../../shared/domain/exceptions/Exception";
-import UserNickname from "../UserNickname";
+import { Exception } from '../../../shared/domain/exceptions/Exception';
+import UserNickname from '../UserNickname';
 
-export default class UserNicknameLenghtExceeded extends Error implements Exception {
+export default class UserNicknameLenghtExceeded
+  extends Error
+  implements Exception
+{
   readonly kind: string = 'BAD_REQUEST';
 
   constructor(value: string) {
-    super(`Nicknames can't be longer than ${UserNickname.MAX_LENGTH} => <${value}>`);
+    super(
+      `Nicknames can't be longer than ${UserNickname.MAX_LENGTH} => <${value}>`
+    );
   }
 }
