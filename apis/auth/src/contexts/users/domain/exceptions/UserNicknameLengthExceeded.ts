5fc2e6e1bcd0568b3fc8f4084908a2dc1e30e2dc
leivaa <adrianleiva212@gmail.com>
2023-02-21 16:46:59 +0100

    feat: exception for exceding max nickname characters

---
+++ apis/auth/src/contexts/users/domain/exceptions/UserNicknameLengthExceeded.ts
@@ -0,0 +1,10 @@
+import { Exception } from "../../../shared/domain/exceptions/Exception";
+import UserNickname from "../UserNickname";
+
+export default class UserNicknameLenghtExceeded extends Error implements Exception {
+  readonly kind: string = 'BAD_REQUEST';
+
+  constructor(value: string) {
+    super(`Nicknames can't be longer than ${UserNickname.MAX_LENGTH} => <${value}>`);
+  }
+}
