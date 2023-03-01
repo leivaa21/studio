20c87bb1e8a7f10afca08abbe5d6f0b5cb8dd60b
leivaa <adrianleiva212@gmail.com>
2023-03-01 16:21:52 +0100

    feat: colors for loggers

---
+++ packages/api-utils/src/loggers/colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+	red: '\x1B[38;2;255;0;0m',
+	blue: '\x1B[38;2;0;0;255m',
+	green: '\x1B[38;2;0;255;0m',
+	cyan: '\x1B[38;2;0;255;255m',
+	yellow: '\x1B[38;2;255;255;0m',
+	orange: '\x1B[38;2;255;150;0m',
+	reset: '\x1b[0m',
+};
