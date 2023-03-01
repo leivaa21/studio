31f351b252f5dbad72d628879d1c18984a4201bc
leivaa <adrianleiva212@gmail.com>
2023-03-01 22:24:42 +0100

    feat: added ts-loader & babel-loader

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['next/babel'],
+          },
+        },
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+      exclude: /node_modules/,
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
