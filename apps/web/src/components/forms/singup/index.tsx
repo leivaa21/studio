2f83f3cec7bd87f73ff687c9f2a17ee661aca835
leivaa <adrianleiva212@gmail.com>
2023-03-13 18:21:02 +0100

    wip: singup page

--- apps/web/src/components/forms/singup/index.tsx
+++ apps/web/src/components/forms/singup/index.tsx
@@ -2,18 +2,34 @@ import React from "react";
 
 import Button from "@studio/ui/components/interactivity/cta/button";
 import Form, {FormBody, FormTextInput, FormHeader, FormHint} from '@studio/ui/components/interactivity/form';
+
+import styles from './singup.module.scss'
 import Link from "next/link";
 
-export const SignInForm = () => {
+export const SignUpForm = () => {
+  const [nickname, setNickname] = React.useState<string>('');
   const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
 
+  const onSubmitSignUp = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    console.log({nickname, email, password})
+  }
+
   return (
-    <Form style={{ width: '100%' }}>
+    <Form className={styles.SingUpForm}>
       <FormHeader>
         <h1><b>Join now</b> and get access to <b>all</b> the courses!🚀</h1>
       </FormHeader>
       <FormBody>
+        <FormTextInput
+          Name="Nickname"
+          placeholder="nickname"
+          type="text"
+          value={nickname}
+          onChange={(e) => setNickname(e.currentTarget.value)}
+        />
         <FormTextInput
           Name="Email"
           placeholder="email"
@@ -28,10 +44,10 @@ export const SignInForm = () => {
           value={password}
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
-        <Button Label="sign in!" Size="Medium" Type="Primary" />
+        <Button Label="sign up!" Size="Medium" Type="Primary" onClick={onSubmitSignUp} />
         <FormHint>
-          <p>You dont have account yet?</p>
-          <Link href="/singup" style={{ cursor: 'pointer' }}>Sing up here!</Link>
+          <p>Missclicked here?, go back!</p>
+          <Link href="/" style={{ cursor: 'pointer' }}>Back to landing page!</Link>
         </FormHint>
       </FormBody>
     </Form>
