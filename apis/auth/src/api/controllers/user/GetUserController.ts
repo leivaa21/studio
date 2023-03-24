25fd7710dae404bde9364b1f00b2698a789ecef7
leivaa <adrianleiva212@gmail.com>
2023-03-24 19:36:10 +0100

    feat: following contract in auth api

--- apis/auth/src/api/controllers/user/GetUserController.ts
+++ apis/auth/src/api/controllers/user/GetUserController.ts
@@ -1,11 +1,12 @@
 import { StatusCode } from '@studio/api-utils/http';
+import { GetUserResponse } from '@studio/commons/dist/contracts/user/GetUserContracts';
 import { JsonController, HttpCode, Get, QueryParam } from 'routing-controllers';
 
 @JsonController('/user')
 export class GetUserController {
   @Get('')
   @HttpCode(StatusCode.CREATED)
-  async registerWithBasicCredentials(@QueryParam('email') email: string) {
-    return { id: email };
+  async GetUser(@QueryParam('email') email: string): Promise<GetUserResponse> {
+    return { id: 'SomeIdHere', nickname: 'SomeNicknameHere' };
   }
 }
