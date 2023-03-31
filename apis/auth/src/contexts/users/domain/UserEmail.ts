ee1c6df4d125907e60d928a5a491f63eb9e7a379
leivaa <adrianleiva212@gmail.com>
2023-03-31 18:13:01 +0200

    feat: initialize & save  email always as lowercase

--- apis/auth/src/contexts/users/domain/UserEmail.ts
+++ apis/auth/src/contexts/users/domain/UserEmail.ts
@@ -1,6 +1,10 @@
 import { ValueObject } from '@studio/commons';
 
 export class UserEmail extends ValueObject<string> {
+  constructor(value: string) {
+    super(value.toLowerCase());
+  }
+
   static of(value: string): UserEmail {
     return new UserEmail(value);
   }
