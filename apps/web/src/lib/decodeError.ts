d9928a7b7a8827b1e427d049815acc107be41381
leivaa <adrianleiva212@gmail.com>
2023-03-31 18:34:54 +0200

    feat: assert secure password

--- apps/web/src/lib/decodeError.ts
+++ apps/web/src/lib/decodeError.ts
@@ -13,6 +13,23 @@ const errorCodeToErrorMessage = new Map<string, string>([
   [ErrorCodes.InvalidPassword, 'The password is invalid :('],
   [ErrorCodes.UserNotFound, 'Unable to find that user :('],
   [ErrorCodes.BadRequest, 'Something went wrong with the request :$'],
+  [ErrorCodes.PasswordTooShort, 'Password should be longer than 8 characters'],
+  [
+    ErrorCodes.PasswordShouldContainLowercase,
+    'Password should contain atleast one lowercase',
+  ],
+  [
+    ErrorCodes.PasswordShouldContainUppercase,
+    'Password should contain atleast one uppercase',
+  ],
+  [
+    ErrorCodes.PasswordShouldContainNumber,
+    'Password should contain atleast one number',
+  ],
+  [
+    ErrorCodes.PasswordShouldContainSymbol,
+    'Password should contain atleast one symbol',
+  ],
 ]);
 
 export function decodeError(errorCode: string): string {
