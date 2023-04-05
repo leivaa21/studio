5167bb6251fe076faf3bc04066b5136b2e95a9b3
leivaa <adrianleiva212@gmail.com>
2023-04-05 15:35:19 +0200

    feat: emails now are validated with regex expresion

--- apis/auth/src/contexts/users/domain/UserEmail.ts
+++ apis/auth/src/contexts/users/domain/UserEmail.ts
@@ -1,11 +1,23 @@
 import { ValueObject } from '@studio/commons';
+import { InvalidEmailError } from './errors/InvalidEmail';
 
 export class UserEmail extends ValueObject<string> {
   constructor(value: string) {
     super(value.toLowerCase());
   }
 
+  static validate(value: string) {
+    const emailRegex = new RegExp(
+      /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+      'gm'
+    );
+    if (!emailRegex.test(value)) {
+      throw InvalidEmailError.causeEmailIsNotValid(value);
+    }
+  }
+
   static of(value: string): UserEmail {
+    this.validate(value);
     return new UserEmail(value);
   }
 }
