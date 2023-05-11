2a06a748e5ca268bc19ec7f35d64674ee5482f2b
Leivaa <adrianleiva@genially.com>
2023-05-11 18:47:19 +0200

    test: registerNewUserGoogleCredentials usecase

---
+++ apis/auth/test/unit/contexts/user/application/commands/RegisterNewUser/RegisterNewUserGoogleCredentials.spec.ts
@@ -0,0 +1,48 @@
+import { UserEmail } from '../../../../../../../src/contexts/users/domain/UserEmail';
+import { InMemoryUserRepository } from '../../../../../../../src/contexts/users/infrastructure/persistance/InMemoryUserRepository';
+import { UserBuilder } from '../../../../../../helpers/builders/user/UserBuilder';
+import { StringMother } from '../../../../../../helpers/object-mother/StringMother';
+import { EmailMother } from '../../../../../../helpers/object-mother/UserEmailMother';
+import { RegisterNewUserGoogleCredentials } from '../../../../../../../src/contexts/users/application/commands/RegisterNewUser/RegisterNewUserGoogleCredentials';
+import { GoogleId } from '../../../../../../../src/contexts/users/domain/GoogleId';
+
+describe('Register New User with Basic Credentials', () => {
+  it('Should create a valid user', async () => {
+    const command = {
+      email: EmailMother.random().value,
+      googleId: StringMother.random(),
+    };
+
+    const userRepository = new InMemoryUserRepository([]);
+
+    const useCase = new RegisterNewUserGoogleCredentials(userRepository);
+
+    useCase.execute(command);
+
+    const user = await userRepository.findByEmail(UserEmail.of(command.email));
+
+    expect(user).toBeDefined();
+    expect(user?.nickname.value).toStrictEqual(command.email);
+  });
+
+  it('Should not create a user that is already registered', async () => {
+    const googleId = GoogleId.of(StringMother.random());
+
+    const alreadyPersistedUser = UserBuilder.aGoogleCredentialsUser()
+      .withGoogleId(googleId)
+      .build();
+
+    const command = {
+      googleId: googleId.value,
+      email: alreadyPersistedUser.email.value,
+    };
+
+    const userRepository = new InMemoryUserRepository([alreadyPersistedUser]);
+
+    const useCase = new RegisterNewUserGoogleCredentials(userRepository);
+
+    await useCase.execute(command);
+
+    expect(userRepository.users.length).toBe(1);
+  });
+});
