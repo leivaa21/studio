1a7d2a475331362dcf72f80c71ee8d9724313323
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-07-02 22:54:03 +0200

    [API - Courses] Authorization checker & Current user for course module (#26)
    * deps: added some necesary deps for jwt authentication
    
    * feat: added a simplified version of auth checker for courses (just reading the jwt)

--- apis/courses/src/api/config/env.ts
+++ apis/courses/src/api/config/env.ts
@@ -10,6 +10,16 @@ function getEnvVariableOrUndefined<T>(varName: string): T | undefined {
   return getEnvVariableOrDefault<T | undefined>(varName, undefined);
 }
 
+function getEnvVariableOrThrow(varName: string): string {
+  const variable = process.env[varName];
+
+  if (!variable) {
+    throw new Error(`<${varName}> Should be defined in .env file`);
+  }
+
+  return variable;
+}
+
 export const env = {
   courses: {
     port: getEnvVariableOrDefault<number>('PORT', 5002),
@@ -21,5 +31,8 @@ export const env = {
     db: getEnvVariableOrDefault<string>('MONGO_DATABASE', 'studio-courses'),
     user: getEnvVariableOrUndefined<string>('MONGO_USER'),
     pass: getEnvVariableOrUndefined<string>('MONGO_PASS'),
+  },
+  jwt: {
+    secret: getEnvVariableOrThrow('JWT_SECRET'),
   },
 };
