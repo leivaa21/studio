eabe62f77e286637514531853a4b4e15e4ed1ab9
Leivaa <adrianleiva@genially.com>
2023-07-11 20:59:40 +0200

    feat: web endpoint to get courses

---
+++ apps/web/src/pages/api/courses/authored.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { SafeControllerHandling } from '../../../lib/controllers/SafeControllerHandling';
+import { CoursesApiService } from '../../../contexts/shared/infrastructure/ApiClients/CoursesApiService';
+import { CourseInfoResponse } from '@studio/commons';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    return res
+      .status(404)
+      .json({ status: 404, message: `Cannot ${req.method} ${req.url}` });
+  }
+
+  const authToken = req.headers.authorization;
+  const params = new Map([
+    ['page', req.query.page as string],
+    ['pageSize', req.query.pageSize as string],
+    ['title', req.query.title as string],
+  ]);
+
+  const coursesApiService = new CoursesApiService();
+
+  await SafeControllerHandling(res, async () => {
+    const courses = await coursesApiService.get<CourseInfoResponse>(
+      '/courses',
+      params,
+      authToken
+    );
+
+    res.status(200).end(courses);
+  });
+}
