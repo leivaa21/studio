4648cd31a89098e246f6f7e6bd32c8a3776bdd66
Leivaa <adrianleiva@genially.com>
2023-07-11 20:41:25 +0200

    feat: endpoint on courses api

---
+++ apis/courses/src/api/controllers/courses/GetMyCoursesPaginatedController.ts
@@ -0,0 +1,50 @@
+import { StatusCode } from '@studio/api-utils';
+import {
+  Authorized,
+  BadRequestError,
+  Body,
+  CurrentUser,
+  Get,
+  HttpCode,
+  JsonController,
+  OnUndefined,
+  Param,
+  Post,
+} from 'routing-controllers';
+import { Injectable } from '@studio/dependency-injection';
+import { CreateNewCourseCommand } from '../../../contexts/courses/application/commands/CreateNewCourse';
+import { User } from '../../auth/user';
+import { InMemoryQueryBus } from '../../../contexts/shared/infrastructure/QueryBus/InMemoryQueryBus';
+import { QueryBus } from '../../../contexts/shared/domain/QueryBus';
+import { GetMyCoursesPaginatedQuery } from '../../../contexts/courses/application/queries/GetMyCoursesPaginated';
+import { Course } from '../../../contexts/courses/domain/Course';
+
+@Injectable({
+  dependencies: [InMemoryQueryBus],
+})
+@JsonController('/courses/authored')
+export class CreateNewCourseController {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @Get()
+  @HttpCode(StatusCode.OK)
+  @OnUndefined(StatusCode.OK)
+  @Authorized()
+  public async execute(
+    @CurrentUser({ required: true }) user: User,
+    @Param('page') page = 0,
+    @Param('count') count = 0,
+    @Param('title') title: string
+  ) {
+    await this.queryBus.dispatch<GetMyCoursesPaginatedQuery, Course[]>(
+      new GetMyCoursesPaginatedQuery({
+        authorId: user.id,
+        pageSize: count,
+        page,
+        with: {
+          title: title,
+        },
+      })
+    );
+  }
+}
