6db9e87f4ee6029272816b4b9ac97c8d2201a343
Leivaa <adrianleiva@genially.com>
2023-07-11 20:35:46 +0200

    feat: queryBus and query mapped to queryBus

--- apis/courses/src/api/mapping/courses/index.ts
+++ apis/courses/src/api/mapping/courses/index.ts
@@ -1,11 +1,2 @@
-import { DependencyContainer } from '@studio/dependency-injection';
-import { CommandBus } from '../../../contexts/shared/domain/CommandBus';
-import { InMemoryCommandBus } from '../../../contexts/shared/infrastructure/CommandBus/InMemoryCommandBus';
-import {
-  CreateNewCourse,
-  CreateNewCourseCommand,
-} from '../../../contexts/courses/application/commands/CreateNewCourse';
-
-const commandBus = DependencyContainer.get<CommandBus>(InMemoryCommandBus);
-
-commandBus.subscribe(CreateNewCourseCommand, CreateNewCourse);
+import './commands';
+import './queries';
