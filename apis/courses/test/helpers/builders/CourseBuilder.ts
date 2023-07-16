3f14caf55583b9fd15e75ae4b0cb157e8fa9d8fa
Leivaa <adrianleiva@genially.com>
2023-07-16 22:54:10 +0200

    fix: course builder was missing tags new parameter

--- apis/courses/test/helpers/builders/CourseBuilder.ts
+++ apis/courses/test/helpers/builders/CourseBuilder.ts
@@ -2,6 +2,7 @@ import { AuthorId } from '../../../src/contexts/courses/domain/AuthorId';
 import { Course } from '../../../src/contexts/courses/domain/Course';
 import { CourseDescription } from '../../../src/contexts/courses/domain/CourseDescription';
 import { CourseId } from '../../../src/contexts/courses/domain/CourseId';
+import { CourseTag } from '../../../src/contexts/courses/domain/CourseTag';
 import { CourseTitle } from '../../../src/contexts/courses/domain/CourseTitle';
 import { StringMother } from '../object-mother/StringMother';
 import { Builder } from './builder';
@@ -9,6 +10,7 @@ import { Builder } from './builder';
 export class CourseBuilder implements Builder<Course> {
   private _id: CourseId = CourseId.random();
   private _title: CourseTitle = CourseTitle.of(StringMother.random());
+  private _tags: CourseTag[] = [];
   private _description: CourseDescription = CourseDescription.of(
     StringMother.random()
   );
@@ -19,6 +21,7 @@ export class CourseBuilder implements Builder<Course> {
     return new Course({
       id: this._id,
       title: this._title,
+      tags: this._tags,
       description: this._description,
       authorId: this._authorId || AuthorId.random(),
       createdAt: this._createdAt,
