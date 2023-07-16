f31f9e995da7a83925e4f4777a40d516efe5afe1
Leivaa <adrianleiva@genially.com>
2023-07-16 22:41:14 +0200

    feat: implemented needed methods on mongo repository

--- apis/courses/src/contexts/courses/infrastructure/persistance/mongo/MongoCourseRepository.ts
+++ apis/courses/src/contexts/courses/infrastructure/persistance/mongo/MongoCourseRepository.ts
@@ -7,6 +7,8 @@ import { CourseData } from './CourseData';
 import { CourseSchemaFactory } from './CourseSchemaFactory';
 import { CourseCriteria } from '../../../domain/criteria/CourseCriteria';
 import { MongoCriteriaConverter } from '../../../../shared/infrastructure/mongo/MongoCriteriaConverter';
+import { Nullable } from '../../../../shared/domain/Nullable';
+import { CourseId } from '../../../domain/CourseId';
 
 @Injectable({ dependencies: [CourseSchemaFactory, MongoCriteriaConverter] })
 export class MongoCourseRepository
@@ -20,7 +22,11 @@ export class MongoCourseRepository
     super(courseSchemaFactory);
   }
 
-  async matching(criteria: CourseCriteria): Promise<Course[]> {
+  protected model() {
+    return CourseModel;
+  }
+
+  public async matching(criteria: CourseCriteria): Promise<Course[]> {
     const query = this.criteriaConverter.convert(criteria);
     const documents = await this.model()
       .find(query.filter)
@@ -33,11 +39,20 @@ export class MongoCourseRepository
     );
   }
 
-  protected model() {
-    return CourseModel;
+  public async create(course: Course): Promise<void> {
+    await this.persist(course.id.value, course);
   }
 
-  async create(course: Course): Promise<void> {
-    await this.persist(course.id.value, course);
+  public async findById(id: CourseId): Promise<Nullable<Course>> {
+    const document = await this.model().findById(id.value);
+
+    return document
+      ? this.entitySchemaFactory.createEntityFromSchema(document)
+      : null;
+  }
+
+  public async update(course: Course): Promise<void> {
+    const document = this.entitySchemaFactory.createSchemaFromEntity(course);
+    await this.model().findByIdAndUpdate(document._id, document);
   }
 }
