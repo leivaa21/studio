127ab0fa411baf150066d95c437fa84a2f117600
Leivaa <adrianleiva@genially.com>
2023-07-18 00:01:32 +0200

    feat: added updatedAt property to courses

--- apis/courses/src/contexts/courses/domain/Course.ts
+++ apis/courses/src/contexts/courses/domain/Course.ts
@@ -15,6 +15,7 @@ export interface CourseParams {
   readonly tags: CourseTags;
   readonly description: CourseDescription;
   readonly createdAt: Date;
+  readonly updatedAt: Date;
 }
 
 export interface CoursePrimitives {
@@ -24,6 +25,7 @@ export interface CoursePrimitives {
   readonly tags: string[];
   readonly description: string;
   readonly createdAt: Date;
+  readonly updatedAt: Date;
 }
 
 export class Course extends AggregateRoot {
@@ -33,14 +35,16 @@ export class Course extends AggregateRoot {
   private _tags: CourseTags;
   private _description: CourseDescription;
   public readonly createdAt: Date;
+  private _updatedAt: Date;
 
   constructor({
     id,
     authorId,
     title,
+    tags,
     description,
     createdAt,
-    tags,
+    updatedAt,
   }: CourseParams) {
     super();
     this.id = id;
@@ -49,6 +53,7 @@ export class Course extends AggregateRoot {
     this._tags = tags;
     this._description = description;
     this.createdAt = createdAt;
+    this._updatedAt = updatedAt;
   }
 
   static new({
@@ -78,6 +83,7 @@ export class Course extends AggregateRoot {
       tags,
       description,
       createdAt: new Date(),
+      updatedAt: new Date(),
     });
 
     course.commit(courseWasCreatedEvent);
@@ -93,6 +99,7 @@ export class Course extends AggregateRoot {
     if (this._title.equals(title)) return;
 
     this._title = title;
+    this._updatedAt = new Date();
 
     const renamedEvent = CourseWasRenamedEvent.fromPrimitives({
       aggregateId: this.id.value,
@@ -108,12 +115,14 @@ export class Course extends AggregateRoot {
     if (this._description.equals(description)) return;
 
     this._description = description;
+    this._updatedAt = new Date();
   }
 
   public updateTags(tags: CourseTags) {
     if (this._tags.equals(tags)) return;
 
     this._tags = tags;
+    this._updatedAt = new Date();
   }
 
   public get title(): CourseTitle {
@@ -128,6 +137,10 @@ export class Course extends AggregateRoot {
     return this._tags;
   }
 
+  public get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
   public static fromPrimitives(coursePrimitives: CoursePrimitives): Course {
     return new Course({
       id: CourseId.of(coursePrimitives.id),
@@ -138,6 +151,7 @@ export class Course extends AggregateRoot {
       ),
       description: CourseDescription.of(coursePrimitives.description),
       createdAt: coursePrimitives.createdAt,
+      updatedAt: coursePrimitives.updatedAt,
     });
   }
 
@@ -149,6 +163,7 @@ export class Course extends AggregateRoot {
       tags: this.tags.values,
       description: this.description.value,
       createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
     };
   }
 }
--- apis/courses/src/contexts/courses/infrastructure/persistance/mongo/CourseData.ts
+++ apis/courses/src/contexts/courses/infrastructure/persistance/mongo/CourseData.ts
@@ -5,4 +5,5 @@ export interface CourseData {
   description: string;
   tags: string[];
   createdAt: Date;
+  updatedAt: Date;
 }
