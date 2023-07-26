8f198b32e7e9d210652a54915e72b662d3330a16
Leivaa <adrianleiva@genially.com>
2023-07-27 00:34:12 +0200

    feat: added delete button on lesson card with confirm modal

--- apps/web/src/components/creator/course/lessons/CourseLessonCard.tsx
+++ apps/web/src/components/creator/course/lessons/CourseLessonCard.tsx
@@ -1,6 +1,9 @@
 import Button from '@studio/ui/components/interactivity/cta/button';
 import styles from '../course.module.scss';
 import { BsArrowDownShort, BsArrowUpShort } from 'react-icons/bs';
+import { Modal } from '@studio/ui/components/modal';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export interface LessonCardParams {
   lesson: { title: string; id: string; courseId: string };
@@ -15,6 +18,14 @@ export function CourseLessonCard({
   lessonIndex,
   lessonCount,
 }: LessonCardParams) {
+  const [deleteModalShown, setDeleteModalShown] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const handleDeleteLesson = () => {
+    router.reload();
+  };
+
   return (
     <div className={styles.lessonCard} key={key}>
       <div className={styles.lessonOrderControls}>
@@ -39,7 +50,28 @@ export function CourseLessonCard({
           Link
           href={`/creator/course/${lesson.courseId}/lesson/${lesson.id}/edit`}
         />
+        <Button
+          Type="Cancel"
+          Size="Small"
+          Label="REMOVE"
+          onClick={() => setDeleteModalShown(true)}
+        />
       </div>
+      <Modal
+        isShown={deleteModalShown}
+        closeFunction={() => setDeleteModalShown(false)}
+        title="Remove lesson"
+      >
+        <div className={styles.removeLessonModal}>
+          <h4>Are you sure to delete lesson &quot;{lesson.title}&quot;</h4>
+          <Button
+            Type="Cancel"
+            Size="Medium"
+            Label="REMOVE"
+            onClick={handleDeleteLesson}
+          />
+        </div>
+      </Modal>
     </div>
   );
 }
