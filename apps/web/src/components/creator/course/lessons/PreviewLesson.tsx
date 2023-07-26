2f50c3d53e15d1a631c2621ca11fdcacd72bc976
Leivaa <adrianleiva@genially.com>
2023-07-26 19:37:11 +0200

    feat: added preview for lesson

--- apps/web/src/components/creator/course/lessons/PreviewLesson.tsx
+++ apps/web/src/components/creator/course/lessons/PreviewLesson.tsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
 import { getLessonById } from '../../../../contexts/lessons/aplication/GetLessonById';
 
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+
+import styles from '../course.module.scss';
+
 export function PreviewLesson({ lessonId }: { lessonId: string }) {
   const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
+  const [content, setContent] =
+    useState<
+      MDXRemoteSerializeResult<Record<string, unknown>, Record<string, unknown>>
+    >();
 
   useEffect(() => {
+    if (!lessonId) return;
     getLessonById(lessonId).then((lesson) => {
       setTitle(lesson.title);
-      setContent(lesson.content);
+      serialize(lesson.content, { mdxOptions: { development: true } }).then(
+        (mdxSource) => setContent(mdxSource)
+      );
     });
-  });
+  }, [lessonId]);
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{content}</p>
+    <div className={styles.lessonPreview}>
+      <h3 className={styles.lessonTitle}>{title}</h3>
+      {content ? <MDXRemote {...content} /> : undefined}
     </div>
   );
 }
