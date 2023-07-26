c7b13565276f0a5a9d95498ff4bbd95122528494
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-07-26 19:44:33 +0200

    [Courses - Creator] Lesson previews (#46)
    * feat: added endpoint for getting lesson by id
    
    * feat: connected endpoint to frontend & wip of preview lesson page
    
    * deps: added next-mdx-remote
    
    * feat: added preview for lesson

---
+++ apis/courses/src/contexts/lessons/application/queries/GetLessonById.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@studio/dependency-injection';
+import { MongoLessonRepository } from '../../infrastructure/persistance/mongo/MongoLessonRepository';
+import { QueryHandler } from '../../../shared/application/QueryHandler';
+import { Lesson } from '../../domain/Lesson';
+import { LessonRepository } from '../../domain/LessonRepository';
+import { LessonFinder } from '../services/LessonFinder';
+import { LessonId } from '../../domain/LessonId';
+
+export class GetLessonByIdQuery {
+  public readonly lessonId: string;
+
+  constructor(args: { lessonId: string }) {
+    this.lessonId = args.lessonId;
+  }
+}
+
+@Injectable({
+  dependencies: [MongoLessonRepository],
+})
+export class GetLessonById implements QueryHandler<GetLessonByIdQuery, Lesson> {
+  private readonly lessonFinder: LessonFinder;
+
+  public constructor(lessonRepository: LessonRepository) {
+    this.lessonFinder = new LessonFinder(lessonRepository);
+  }
+  public async execute(query: GetLessonByIdQuery): Promise<Lesson> {
+    const lessonId = LessonId.of(query.lessonId);
+
+    return this.lessonFinder.findByIdOrThrow(lessonId);
+  }
+}
--- apis/courses/src/contexts/lessons/domain/LessonRepository.ts
+++ apis/courses/src/contexts/lessons/domain/LessonRepository.ts
@@ -1,7 +1,10 @@
 import { CourseId } from '../../courses/domain/CourseId';
+import { Nullable } from '../../shared/domain/Nullable';
 import { Lesson } from './Lesson';
+import { LessonId } from './LessonId';
 
 export interface LessonRepository {
   create(lesson: Lesson): Promise<void>;
   findByCourseId(courseId: CourseId): Promise<Lesson[]>;
+  findById(lessonId: LessonId): Promise<Nullable<Lesson>>;
 }
