d3eb2a937b68dd2e030bd9aa6a47d8cdb56165e2
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-07-30 23:38:16 +0200

    [Courses - Creator] Reordering lessons on course lesson list (#53)
    * feat: added web endpoints
    
    * feat: connected front with endpoints and added modals
    
    * fix: better naming for performing action modal class

--- apps/web/src/components/creator/course/lessons/CourseLessonCard.tsx
+++ apps/web/src/components/creator/course/lessons/CourseLessonCard.tsx
@@ -6,6 +6,8 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { deleteLesson } from '../../../../contexts/lessons/aplication/DeleteLesson';
 import { getAuthTokenCookie } from '../../../../lib/cookieUtils';
+import { reorderLessonUp } from '../../../../contexts/lessons/aplication/ReorderLessonUp';
+import { reorderLessonDown } from '../../../../contexts/lessons/aplication/ReorderLessonDown';
 
 export interface LessonCardParams {
   lesson: { title: string; id: string; courseId: string };
@@ -21,6 +23,10 @@ export function CourseLessonCard({
   lessonCount,
 }: LessonCardParams) {
   const [deleteModalShown, setDeleteModalShown] = useState<boolean>(false);
+  const [moveUpLessonModalShown, setMoveUpLessonModal] =
+    useState<boolean>(false);
+  const [moveDownLessonModalShown, setMoveDownLessonModal] =
+    useState<boolean>(false);
 
   const router = useRouter();
 
@@ -29,12 +35,26 @@ export function CourseLessonCard({
     router.reload();
   };
 
+  const handleMoveUpLesson = async () => {
+    await reorderLessonUp(lesson.id, getAuthTokenCookie() || '');
+    router.reload();
+  };
+
+  const handleMoveDownLesson = async () => {
+    await reorderLessonDown(lesson.id, getAuthTokenCookie() || '');
+    router.reload();
+  };
+
   return (
     <div className={styles.lessonCard} key={key}>
       <div className={styles.lessonOrderControls}>
-        <BsArrowUpShort className={lessonIndex === 0 ? styles.hide : ''} />
+        <BsArrowUpShort
+          className={lessonIndex === 0 ? styles.hide : ''}
+          onClick={() => setMoveUpLessonModal(true)}
+        />
         <BsArrowDownShort
           className={lessonIndex === lessonCount - 1 ? styles.hide : ''}
+          onClick={() => setMoveDownLessonModal(true)}
         />
       </div>
       <h4 className={styles.lessonTitle}>{lesson.title}</h4>
@@ -61,12 +81,42 @@ export function CourseLessonCard({
         />
       </div>
       <Modal
+        isShown={moveUpLessonModalShown}
+        closeFunction={() => setMoveUpLessonModal(false)}
+        title="Move lesson up!"
+      >
+        <div className={styles.performActionModal}>
+          <h4>Are you sure to move lesson &quot;{lesson.title}&quot; up?</h4>
+          <Button
+            Type="Primary"
+            Size="Medium"
+            Label="MOVE UP!"
+            onClick={handleMoveUpLesson}
+          />
+        </div>
+      </Modal>
+      <Modal
+        isShown={moveDownLessonModalShown}
+        closeFunction={() => setMoveDownLessonModal(false)}
+        title="Move lesson down!"
+      >
+        <div className={styles.performActionModal}>
+          <h4>Are you sure to move lesson &quot;{lesson.title}&quot; down?</h4>
+          <Button
+            Type="Primary"
+            Size="Medium"
+            Label="MOVE DOWN!"
+            onClick={handleMoveDownLesson}
+          />
+        </div>
+      </Modal>
+      <Modal
         isShown={deleteModalShown}
         closeFunction={() => setDeleteModalShown(false)}
         title="Remove lesson"
       >
-        <div className={styles.removeLessonModal}>
-          <h4>Are you sure to delete lesson &quot;{lesson.title}&quot;</h4>
+        <div className={styles.performActionModal}>
+          <h4>Are you sure to delete lesson &quot;{lesson.title}&quot;?</h4>
           <Button
             Type="Cancel"
             Size="Medium"
