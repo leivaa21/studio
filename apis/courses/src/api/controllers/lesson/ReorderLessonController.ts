460bd084638e2549a2331060d50ae46010d04acd
Leivaa <adrianleiva@genially.com>
2023-07-30 23:08:49 +0200

    feat: added reorder lesson down to controller

--- apis/courses/src/api/controllers/lesson/ReorderLessonController.ts
+++ apis/courses/src/api/controllers/lesson/ReorderLessonController.ts
@@ -13,25 +13,42 @@ import { CommandBus } from '../../../contexts/shared/domain/CommandBus';
 import { StatusCode } from '@studio/api-utils';
 import { User } from '../../auth/user';
 import { ReorderLessonUpCommand } from '../../../contexts/lessons/application/commands/ReorderLessonUp';
+import { ReorderLessonDownCommand } from '../../../contexts/lessons/application/commands/ReorderLessonDown';
 
 @Injectable({
   dependencies: [InMemoryCommandBus],
 })
-@JsonController('/lesson')
+@JsonController('/lesson/:id')
 export class ReorderLessonController {
   constructor(private readonly commandBus: CommandBus) {}
 
-  @Put('/:id/up')
+  @Put('/up')
   @HttpCode(StatusCode.OK)
   @OnUndefined(StatusCode.OK)
   @Authorized()
-  public async execute(
+  public async executeUp(
     @CurrentUser({ required: true })
     user: User,
     @Param('id') lessonId: string
   ) {
     await this.commandBus.dispatch<ReorderLessonUpCommand>(
       new ReorderLessonUpCommand({
+        authorId: user.id,
+        lessonId,
+      })
+    );
+  }
+  @Put('/down')
+  @HttpCode(StatusCode.OK)
+  @OnUndefined(StatusCode.OK)
+  @Authorized()
+  public async executeDown(
+    @CurrentUser({ required: true })
+    user: User,
+    @Param('id') lessonId: string
+  ) {
+    await this.commandBus.dispatch<ReorderLessonDownCommand>(
+      new ReorderLessonDownCommand({
         authorId: user.id,
         lessonId,
       })
