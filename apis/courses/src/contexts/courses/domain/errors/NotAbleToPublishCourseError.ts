16003e7c11b933e68c50133a33fbdcfa2a562d6f
Leivaa <adrianleiva@genially.com>
2023-08-01 15:57:32 +0200

    feat: added usecase for publishing course

---
+++ apis/courses/src/contexts/courses/domain/errors/NotAbleToPublishCourseError.ts
@@ -0,0 +1,15 @@
+import { ApiError, ErrorCodes } from '@studio/commons';
+
+export class NotAbleToPublishCourseError extends ApiError {
+  constructor(message: string) {
+    super({
+      apiStatus: 404,
+      kind: 'NOT_FOUND',
+      errorCode: ErrorCodes.CourseNotFound,
+      message,
+    });
+  }
+  public static alreadyPublished(id: string) {
+    return new this(`Course with id = <${id}> is already published`);
+  }
+}
