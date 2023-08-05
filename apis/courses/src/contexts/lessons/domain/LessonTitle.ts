2c0a6d7f67349e3496f4abe75afc0cd56bde12e2
Leivaa <adrianleiva@genially.com>
2023-08-05 21:27:59 +0200

    feat: added invalid lesson title error

--- apis/courses/src/contexts/lessons/domain/LessonTitle.ts
+++ apis/courses/src/contexts/lessons/domain/LessonTitle.ts
@@ -1,13 +1,10 @@
-import {
-  InvalidArgumentError,
-  ValueObject,
-  isLessonTitleValid,
-} from '@studio/commons';
+import { ValueObject, isLessonTitleValid } from '@studio/commons';
+import { InvalidLessonTitleError } from './errors/InvalidLessonTitleError';
 
 export class LessonTitle extends ValueObject<string> {
   static of(value: string): LessonTitle {
     if (!isLessonTitleValid(value)) {
-      throw new InvalidArgumentError(this.name, value);
+      throw new InvalidLessonTitleError(this.name);
     }
 
     return new LessonTitle(value);
