71ebf282fbe7af21457b97bf13052844c66ed7dc
Leivaa <adrianleiva@genially.com>
2023-08-05 17:42:32 +0200

    test: unauthorized testcase for creating new course

--- apis/courses/test/e2e/courses/CreateNewCourse.spec.ts
+++ apis/courses/test/e2e/courses/CreateNewCourse.spec.ts
@@ -90,4 +90,29 @@ describe(`POST ${route}`, () => {
       });
     }
   );
+  it('should not let create a new course if request is not authorized', async () => {
+    const course = new CourseBuilder().build();
+    const body = {
+      title: course.title.value,
+      description: course.description.value,
+      tags: course.tags.values,
+    };
+    const response = await request(app)
+      .post(route)
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(401);
+
+    const authorCourses = await findByAuthorId(course.authorId);
+
+    expect(authorCourses).toBeDefined();
+    expect(authorCourses).toHaveLength(0);
+
+    expect(response.body).toStrictEqual({
+      message: 'Authorization is required for request on POST /courses',
+      status: 401,
+      kind: 'UNAUTHORIZED',
+      errorCode: ErrorCodes.Unauthorized,
+    });
+  });
 });
