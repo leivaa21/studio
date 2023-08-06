edb2ba77b6e0f31ab991914581b217353f3b74ab
Leivaa <adrianleiva@genially.com>
2023-08-06 19:08:32 +0200

    test: covered controller

---
+++ apis/courses/test/e2e/course-subscriptions/CreateCourseSubscription.spec.ts
@@ -0,0 +1,112 @@
+import { StartedTestContainer } from 'testcontainers';
+import request from 'supertest';
+import {
+  connectMongooseToContainer,
+  disconnectMongoTest,
+  initializeMongoContainer,
+} from '../../helpers/test-containers/mongo';
+import { CourseBuilder } from '../../helpers/builders/CourseBuilder';
+import { app } from '../../../src/api/app';
+import { AuthorizationTokenBuilder } from '../../helpers/builders/AuthorizationTokenBuilder';
+import { createCourse } from '../../helpers/persistance/mongo/courses';
+import { ErrorCodes } from '@studio/commons';
+import { UserId } from '../../../src/contexts/course-subscriptions/domain/UserId';
+import { findCourseSubscriptionsByUserId } from '../../helpers/persistance/mongo/course-subscriptions';
+
+let mongoContainer: StartedTestContainer;
+const route = '/course-subscriptions';
+
+beforeAll(async () => {
+  mongoContainer = await initializeMongoContainer();
+  await connectMongooseToContainer(mongoContainer);
+}, 99999);
+
+afterAll(async () => {
+  await disconnectMongoTest();
+  await mongoContainer.stop();
+});
+
+describe(`POST ${route}`, () => {
+  it('should let create a course subscription', async () => {
+    const course = new CourseBuilder().build();
+    const userId = UserId.random();
+
+    await createCourse(course);
+
+    const body = {
+      courseId: course.id.value,
+    };
+
+    await request(app)
+      .post(route)
+      .set(
+        'Authorization',
+        new AuthorizationTokenBuilder().withUserId(userId.value).build()
+      )
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const userCourseSubscriptions = await findCourseSubscriptionsByUserId(
+      userId
+    );
+
+    expect(userCourseSubscriptions).toBeDefined();
+    expect(userCourseSubscriptions).toHaveLength(1);
+  });
+
+  it('should throw BadRequest Error if courseId is missing on body', async () => {
+    const userId = UserId.random();
+
+    const response = await request(app)
+      .post(route)
+      .set(
+        'Authorization',
+        new AuthorizationTokenBuilder().withUserId(userId.value).build()
+      )
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    const userCourseSubscriptions = await findCourseSubscriptionsByUserId(
+      userId
+    );
+    expect(userCourseSubscriptions).toHaveLength(0);
+
+    expect(response.body).toStrictEqual({
+      message: 'courseId is required when subscribing to a course',
+      status: 400,
+      kind: 'BAD_REQUEST',
+      errorCode: ErrorCodes.BadRequest,
+    });
+  });
+  it('should not let create a new course if request is not authorized', async () => {
+    const course = new CourseBuilder().build();
+    const userId = UserId.random();
+
+    await createCourse(course);
+
+    const body = {
+      courseId: course.id.value,
+    };
+
+    const response = await request(app)
+      .post(route)
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(401);
+
+    const userCourseSubscriptions = await findCourseSubscriptionsByUserId(
+      userId
+    );
+
+    expect(userCourseSubscriptions).toHaveLength(0);
+
+    expect(response.body).toStrictEqual({
+      message:
+        'Authorization is required for request on POST /course-subscriptions',
+      status: 401,
+      kind: 'UNAUTHORIZED',
+      errorCode: ErrorCodes.Unauthorized,
+    });
+  });
+});
--- apis/courses/test/helpers/persistance/mongo/course-subscriptions.ts
+++ apis/courses/test/helpers/persistance/mongo/course-subscriptions.ts
@@ -2,6 +2,7 @@ import { CourseSubscription } from '../../../../src/contexts/course-subscription
 import { CourseSubscriptionModel } from '../../../../src/contexts/course-subscriptions/infrastructure/persistance/mongo/CourseSubscriptionSchema';
 import { CourseSubscriptionSchemaFactory } from '../../../../src/contexts/course-subscriptions/infrastructure/persistance/mongo/CourseSubscriptionSchemaFactory';
 import { CourseSubscriptionId } from '../../../../src/contexts/course-subscriptions/domain/CourseSubscriptionId';
+import { UserId } from '../../../../src/contexts/course-subscriptions/domain/UserId';
 
 const schemaFactory = new CourseSubscriptionSchemaFactory();
 
@@ -18,4 +19,14 @@ export async function findCourseSubscriptionById(
   const document = await CourseSubscriptionModel.findById(id.value);
 
   return document ? schemaFactory.createEntityFromSchema(document) : undefined;
+}
+
+export async function findCourseSubscriptionsByUserId(
+  id: UserId
+): Promise<CourseSubscription[]> {
+  const documents = await CourseSubscriptionModel.find({ userId: id.value });
+
+  return documents.map((document) =>
+    schemaFactory.createEntityFromSchema(document)
+  );
 }
