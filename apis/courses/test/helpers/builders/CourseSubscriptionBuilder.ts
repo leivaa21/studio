a629b17867bb91d921e7d90394c50cbb5fbd363b
Leivaa <adrianleiva@genially.com>
2023-08-06 18:12:14 +0200

    test: covered create course subscription usecase

---
+++ apis/courses/test/helpers/builders/CourseSubscriptionBuilder.ts
@@ -0,0 +1,30 @@
+import { CourseId } from '../../../src/contexts/courses/domain/CourseId';
+import { CourseSubscription } from '../../../src/contexts/course-subscriptions/domain/CourseSubscription';
+import { Builder } from './builder';
+import { UserId } from '../../../src/contexts/course-subscriptions/domain/UserId';
+import { CourseSubscriptionId } from '../../../src/contexts/course-subscriptions/domain/CourseSubscriptionId';
+
+export class CourseSubscriptionBuilder implements Builder<CourseSubscription> {
+  private _userId: UserId = UserId.random();
+  private _courseId: CourseId = CourseId.random();
+
+  build(): CourseSubscription {
+    return new CourseSubscription({
+      id: CourseSubscriptionId.random(),
+      userId: this._userId,
+      courseId: this._courseId,
+      subscribedAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  public withUserId(userId: UserId) {
+    this._userId = userId;
+    return this;
+  }
+
+  public withCourseId(courseId: CourseId) {
+    this._courseId = courseId;
+    return this;
+  }
+}
---
+++ apis/courses/test/unit/contexts/course-subscriptions/application/commands/CreateCourseSubscription.spec.ts
@@ -0,0 +1,96 @@
+import { mock, mockReset } from 'jest-mock-extended';
+import { CourseRepository } from '../../../../../../src/contexts/courses/domain/CourseRepository';
+import {
+  CreateCourseSubscription,
+  CreateCourseSubscriptionCommand,
+} from '../../../../../../src/contexts/course-subscriptions/application/commands/CreateCourseSubscription';
+import { EventBus } from '../../../../../../src/contexts/shared/domain/EventBus';
+import { CourseBuilder } from '../../../../../helpers/builders/CourseBuilder';
+import { CourseSubscriptionRepository } from '../../../../../../src/contexts/course-subscriptions/domain/CourseSubscriptionRepository';
+import { UserId } from '../../../../../../src/contexts/course-subscriptions/domain/UserId';
+import { CourseNotFoundError } from '../../../../../../src/contexts/courses/domain/errors/CourseNotFoundError';
+import { CourseSubscriptionBuilder } from '../../../../../helpers/builders/CourseSubscriptionBuilder';
+import { InvalidCourseSubscriptionError } from '../../../../../../src/contexts/course-subscriptions/domain/errors/InvalidCourseSubscriptionError';
+
+describe('Create new course subscription', () => {
+  const courseSubscriptionRepository = mock<CourseSubscriptionRepository>();
+  const courseRepository = mock<CourseRepository>();
+  const eventBus = mock<EventBus>();
+
+  beforeEach(() => {
+    mockReset(courseSubscriptionRepository);
+    mockReset(courseRepository);
+    mockReset(eventBus);
+  });
+
+  it('Should create a new course subscription', async () => {
+    const course = new CourseBuilder().build();
+    const userId = UserId.random();
+
+    const command = new CreateCourseSubscriptionCommand({
+      userId: userId.value,
+      courseId: course.id.value,
+    });
+
+    const useCase = new CreateCourseSubscription(
+      courseSubscriptionRepository,
+      courseRepository,
+      eventBus
+    );
+
+    courseRepository.findById.mockResolvedValue(course);
+    courseSubscriptionRepository.findByUserAndCourse.mockResolvedValue(null);
+
+    await expect(useCase.execute(command)).resolves.not.toThrow();
+    expect(courseSubscriptionRepository.create).toBeCalled();
+    expect(eventBus.publish).toBeCalled();
+  });
+
+  it('Should not create a course subscription if course do not exist', async () => {
+    const course = new CourseBuilder().build();
+    const userId = UserId.random();
+
+    const command = new CreateCourseSubscriptionCommand({
+      userId: userId.value,
+      courseId: course.id.value,
+    });
+
+    const useCase = new CreateCourseSubscription(
+      courseSubscriptionRepository,
+      courseRepository,
+      eventBus
+    );
+
+    courseRepository.findById.mockResolvedValue(null);
+    courseSubscriptionRepository.findByUserAndCourse.mockResolvedValue(null);
+
+    await expect(useCase.execute(command)).rejects.toThrow(CourseNotFoundError);
+  });
+
+  it('Should not create a course subscription if user is already subscribed to that course', async () => {
+    const course = new CourseBuilder().build();
+    const courseSubscription = new CourseSubscriptionBuilder()
+      .withCourseId(course.id)
+      .build();
+
+    const command = new CreateCourseSubscriptionCommand({
+      userId: courseSubscription.userId.value,
+      courseId: course.id.value,
+    });
+
+    const useCase = new CreateCourseSubscription(
+      courseSubscriptionRepository,
+      courseRepository,
+      eventBus
+    );
+
+    courseRepository.findById.mockResolvedValue(course);
+    courseSubscriptionRepository.findByUserAndCourse.mockResolvedValue(
+      courseSubscription
+    );
+
+    await expect(useCase.execute(command)).rejects.toThrow(
+      InvalidCourseSubscriptionError
+    );
+  });
+});
