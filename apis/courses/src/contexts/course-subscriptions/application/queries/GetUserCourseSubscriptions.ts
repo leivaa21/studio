abd9144f4bd629556b1150ae1e61b4c0c71811e4
Leivaa <adrianleiva@genially.com>
2023-08-07 17:09:26 +0200

    feat: get user course subscriptions

---
+++ apis/courses/src/contexts/course-subscriptions/application/queries/GetUserCourseSubscriptions.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@studio/dependency-injection';
+import { QueryHandler } from '../../../shared/application/QueryHandler';
+import { MongoCourseSubscriptionRepository } from '../../infrastructure/persistance/mongo/MongoCourseSubscriptionRepository';
+import { CourseSubscriptionFinder } from '../services/CourseSubscriptionFinder';
+import { CourseSubscriptionRepository } from '../../domain/CourseSubscriptionRepository';
+import { UserId } from '../../domain/UserId';
+import { CourseSubscription } from '../../domain/CourseSubscription';
+
+export class GetUserCourseSubscriptionsQuery {
+  public readonly userId: string;
+
+  public constructor(params: { userId: string }) {
+    this.userId = params.userId;
+  }
+}
+
+@Injectable({
+  dependencies: [MongoCourseSubscriptionRepository],
+})
+export class GetUserCourseSubscriptions
+  implements
+    QueryHandler<GetUserCourseSubscriptionsQuery, CourseSubscription[]>
+{
+  private readonly courseSubscriptionFinder: CourseSubscriptionFinder;
+
+  public constructor(
+    courseSubscriptionRepository: CourseSubscriptionRepository
+  ) {
+    this.courseSubscriptionFinder = new CourseSubscriptionFinder(
+      courseSubscriptionRepository
+    );
+  }
+  public async execute(
+    query: GetUserCourseSubscriptionsQuery
+  ): Promise<CourseSubscription[]> {
+    const userId = UserId.of(query.userId);
+
+    return this.courseSubscriptionFinder.findByUser(userId);
+  }
+}
