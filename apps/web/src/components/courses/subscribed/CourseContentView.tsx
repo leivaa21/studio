9660ba875cfaf0ae144c9fbbb0bf9aeec3e2f8a1
Leivaa <adrianleiva@genially.com>
2023-08-08 16:32:07 +0200

    wip: course main view

---
+++ apps/web/src/components/courses/subscribed/CourseContentView.tsx
@@ -0,0 +1,92 @@
+import styles from '../courses.module.scss';
+import { useCourse } from '../../../hooks/course/useCourse';
+import { MarkdownRenderer } from '../../markdown/renderer';
+import { useCourseAuthor } from '../../../hooks/course/useCourseAuthor';
+import { useCourseLessons } from '../../../hooks/course/useCourseLessons';
+import { LessonInfoResponse } from '@studio/commons';
+import Button from '@studio/ui/components/interactivity/cta/button';
+import { BsCheck } from 'react-icons/bs';
+
+export interface CourseContentViewParams {
+  courseId: string;
+}
+
+export function CourseContentView({ courseId }: CourseContentViewParams) {
+  const course = useCourse(courseId);
+  const author = useCourseAuthor(course?.authorId);
+  const lessons = useCourseLessons(courseId);
+
+  return (
+    <div className={styles.courseContentPreview}>
+      <h2 className={styles.title}>{course?.title}</h2>
+      <span className={styles.authorName}>{author?.nickname}</span>
+      <MarkdownRenderer content={course?.description} />
+      <CourseTags keyPrefix={`${course?.id}-tag`} tags={course?.tags || []} />
+      <CourseLessons
+        keyPrefix={`${course?.id}-lesson`}
+        lessons={lessons || []}
+      />
+    </div>
+  );
+}
+
+export function CourseTags(args: { keyPrefix: string; tags: string[] }) {
+  return (
+    <ul className={styles.tags}>
+      {args.tags.map((tag) => (
+        <CourseTag key={`${args.keyPrefix}-${tag}`} tag={tag} />
+      ))}
+    </ul>
+  );
+}
+
+export function CourseTag({ tag }: { tag: string }) {
+  return <li className={styles.tag}>{tag}</li>;
+}
+
+export function CourseLessons(args: {
+  keyPrefix: string;
+  lessons: LessonInfoResponse[];
+}) {
+  return (
+    <section className={styles.lessonsSection}>
+      <h4 className={styles.lessonsSectionTitle}>Lessons</h4>
+      <ul className={styles.lessons}>
+        {args.lessons.map((lesson) => (
+          <CourseLesson key={`${args.keyPrefix}-${lesson}`} lesson={lesson} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export function CourseLesson({ lesson }: { lesson: LessonInfoResponse }) {
+  const isCompleted = false;
+  return (
+    <li className={`${styles.lesson} ${isCompleted ? styles.completed : ''}`}>
+      <div className={styles.icons}>
+        <LessonAlreadyCompletedIcon isCompleted={isCompleted} />
+      </div>
+      <h5 className={styles.lessonTitle}>{lesson.title}</h5>
+      <div className={styles.controls}>
+        <Button Type="Primary" Label="Take lesson!" Size="Small" />
+      </div>
+    </li>
+  );
+}
+
+export function LessonAlreadyCompletedIcon({
+  isCompleted,
+}: {
+  isCompleted: boolean;
+}) {
+  return (
+    <div
+      className={`${styles.completedIcon} ${
+        isCompleted ? styles.completed : ''
+      }`}
+    >
+      {isCompleted ? <BsCheck /> : undefined}
+    </div>
+  );
+}
