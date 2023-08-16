cdd8fcc75003414bc886c34b17f36efd14cdc01c
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-08-16 16:10:37 +0200

    feat: user info endpoint (#99)

--- packages/commons/src/contracts/user/GetUserContracts.ts
+++ packages/commons/src/contracts/user/GetUserContracts.ts
@@ -6,3 +6,20 @@ export interface GetUserResponse {
 export interface GetUserNicknameResponse {
   nickname: string;
 }
+
+interface EmailBasedCredentials {
+  type: 'BASIC' | 'GOOGLE';
+  email: string;
+}
+interface GithubCredentials {
+  type: 'GITHUB'
+}
+
+type PossibleCredentialsType = EmailBasedCredentials | GithubCredentials;
+
+export interface GetUserInfoResponse {
+  id: string;
+  nickname: string;
+  credentials: PossibleCredentialsType;
+  joinedAt: Date;
+}
