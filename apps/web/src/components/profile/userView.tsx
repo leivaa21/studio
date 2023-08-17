20b546605a34fcb616006804fc541acf01bccfdd
Leivaa <adrianleiva@genially.com>
2023-08-17 22:53:54 +0200

    feat: profile view (not functional yet)

---
+++ apps/web/src/components/profile/userView.tsx
@@ -0,0 +1,146 @@
+import styles from './user.module.scss';
+import { useUserInfo } from '../../hooks/user/useUserInfo';
+import { Fragment } from 'react';
+import Image from 'next/image';
+import { BsPersonFill } from 'react-icons/bs';
+import { GetUserInfoResponse } from '@studio/commons';
+import Button from '@studio/ui/components/interactivity/cta/button';
+import { formatDate } from '../../utils/formatDate';
+
+export interface UserProfileViewParams {
+  userId: string;
+}
+
+export function UserProfileView({ userId }: UserProfileViewParams) {
+  const userInfo = useUserInfo(userId);
+
+  if (!userInfo) return <Fragment />;
+
+  const canModifyNickname = userInfo.credentials.type !== 'GITHUB';
+
+  return (
+    <div className={styles.profileView}>
+      <ul className={styles.properties}>
+        <li className={styles.property}>
+          <div className={styles.field}>
+            <span className={styles.propertyTitle}>Nickname: </span>
+            <span className={styles.propertyValue}>{userInfo.nickname}</span>
+          </div>
+          {canModifyNickname ? (
+            <div className={styles.propertyControls}>
+              <Button Label="Change Nickname" Size="Small" Type="Primary" />
+            </div>
+          ) : (
+            <Fragment />
+          )}
+        </li>
+        <li className={styles.property}>
+          <div className={styles.field}>
+            <span className={styles.propertyTitle}>Joined At: </span>
+            <span className={styles.propertyValue}>
+              {formatDate(new Date(userInfo.joinedAt))}
+            </span>
+          </div>
+        </li>
+        <GenericUserCredentials userInfo={userInfo} />
+      </ul>
+      <div className={styles.userControls}>
+        <Button Label="Delete My Account" Type="Cancel" Size="Small" />
+      </div>
+    </div>
+  );
+}
+
+function GenericUserCredentials({
+  userInfo,
+}: {
+  userInfo: GetUserInfoResponse;
+}) {
+  const renderCrendentialsIdentifier = () => {
+    switch (userInfo.credentials.type) {
+      case 'BASIC':
+        return (
+          <BasicCredentialsIdentifier email={userInfo.credentials.email} />
+        );
+      case 'GITHUB':
+        return <GithubCredentialsIdentifier nickname={userInfo.nickname} />;
+      case 'GOOGLE':
+        return (
+          <GoogleCredentialsIdentifier email={userInfo.credentials.email} />
+        );
+    }
+  };
+
+  const isBasicCredentials = userInfo.credentials.type === 'BASIC';
+
+  return (
+    <Fragment>
+      <li className={styles.property}>
+        <div className={styles.field}>
+          <span className={styles.propertyTitle}>
+            Credentials:{' '}
+            <span className={styles.credentialsType}>
+              ({userInfo.credentials.type.toLowerCase()})
+            </span>
+          </span>
+          {renderCrendentialsIdentifier()}
+        </div>
+        {isBasicCredentials ? (
+          <div className={styles.propertyControls}>
+            <Button Label="Change Email" Size="Small" Type="Primary" />
+          </div>
+        ) : (
+          <Fragment />
+        )}
+      </li>
+      {isBasicCredentials ? (
+        <li className={styles.property}>
+          <div className={styles.changePasswordRow}>
+            <Button Label="Change Password" Size="Small" Type="Primary" />
+          </div>
+        </li>
+      ) : (
+        <Fragment />
+      )}
+    </Fragment>
+  );
+}
+
+function BasicCredentialsIdentifier({ email }: { email: string }) {
+  return (
+    <span className={styles.propertyValue}>
+      <BsPersonFill
+        className={styles.credentailsIcon}
+        aria-label="Basic credentials"
+      />
+      {email}
+    </span>
+  );
+}
+
+function GithubCredentialsIdentifier({ nickname }: { nickname: string }) {
+  return (
+    <a
+      href={`https://github.com/${nickname}`}
+      target="_blank"
+      rel="no-referrer noopener"
+      className={styles.propertyValue}
+    >
+      <span className={styles.credentailsIcon}>
+        <Image src={'/icons/github.svg'} alt="Github Credentials" fill />
+      </span>
+      {nickname}
+    </a>
+  );
+}
+
+function GoogleCredentialsIdentifier({ email }: { email: string }) {
+  return (
+    <span className={styles.propertyValue}>
+      <span className={styles.credentailsIcon}>
+        <Image src={'/icons/google.svg'} alt="Google Credentials" fill />
+      </span>
+      {email}
+    </span>
+  );
+}
---
+++ apps/web/src/pages/profile/index.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { Fragment } from 'react';
+import { Header } from '../../components/header/header';
+import { getAuthTokenCookie } from '../../lib/cookieUtils';
+import { CreatorHeader } from '../../components/creator/header';
+import { useCurrentUser } from '../../hooks/user/useCurrentUser';
+import { UserProfileView } from '../../components/profile/userView';
+
+export default function Panel() {
+  const router = useRouter();
+
+  const user = useCurrentUser();
+
+  useEffect(() => {
+    if (!getAuthTokenCookie()) router.push('/');
+  }, [router]);
+
+  return (
+    <Fragment>
+      <Header />
+      <CreatorHeader title="Profile" />
+      <div className="row">
+        <div className="column">
+          <UserProfileView userId={user?.id || ''} />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
