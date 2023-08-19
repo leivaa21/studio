fb490204791aaf9d9ceea6335974d116bfacc099
Leivaa <adrianleiva@genially.com>
2023-08-19 19:35:07 +0200

    feat: unified error handlers between apis

--- apis/auth/src/api/middlewares/after/ErrorHandler.ts
+++ apis/auth/src/api/middlewares/after/ErrorHandler.ts
@@ -1,6 +1,4 @@
-import { StatusCode } from '@studio/api-utils';
 import { ErrorHandler } from '@studio/api-utils';
-import { ApiError } from '@studio/commons';
 import { Injectable } from '@studio/dependency-injection';
 import { NextFunction, Request, Response } from 'express';
 import {
@@ -17,20 +15,6 @@ export class CustomErrorHandler implements ExpressErrorMiddlewareInterface {
     response: Response,
     next: NextFunction
   ) {
-    const isErrorLaunchedFromDomain =
-      (error as unknown as ApiError).kind !== undefined;
-    if (isErrorLaunchedFromDomain) {
-      const domainError = error as unknown as ApiError;
-      response
-        .status(StatusCode[domainError.kind] || StatusCode.BAD_REQUEST)
-        .json({
-          message: domainError.message,
-          apiStatus: StatusCode[domainError.kind] || StatusCode.BAD_REQUEST,
-          errorCode: domainError.errorCode,
-          kind: domainError.kind,
-        });
-    } else {
-      ErrorHandler(error, request, response, next);
-    }
+    ErrorHandler(error, request, response, next);
   }
 }
