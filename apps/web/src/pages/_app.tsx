6dd6b981da8043834665de0a6f805ac0880c3155
Leivaa <adrianleiva@genially.com>
2023-08-19 21:05:08 +0200

    feat: added error boundary

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import { ShowErrorBoundary } from '../components/ErrorBoundary';
 import '../styles/globals.scss';
 import '../styles/rewriteMdxStyles.scss';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary FallbackComponent={ShowErrorBoundary}>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
