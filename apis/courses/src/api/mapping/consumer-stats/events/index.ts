fd01aae9507c3e6ae3f07b696398746be45948c0
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-08-23 23:43:57 +0200

    [Courses - Consumer Stats] Managing stats with events  (#120)
    * feat: managing course subscriptions stats
    
    * feat: managing course subscription completed stats
    
    * feat: loading event handlers for consumers stats
    
    * fix: right dependencies on decorator
    
    * test: fixed affected e2e tests
    
    * test: infrastructure testing

---
+++ apis/courses/src/api/mapping/consumer-stats/events/index.ts
@@ -0,0 +1,41 @@
+import { DependencyContainer } from '@studio/dependency-injection';
+import { EventBus } from '../../../../contexts/shared/domain/EventBus';
+import { InMemoryAsyncEventBus } from '../../../../contexts/shared/infrastructure/EventBus/InMemoryAsyncEventBus';
+import { DomainEventSubscriber } from '../../../../contexts/shared/domain/DomainEventSubscriber';
+import { CourseSubscriptionWasCreatedEvent } from '../../../../contexts/course-subscriptions/domain/events/CourseSubscriptionWasCreated';
+import { CourseSubscriptionWasDeletedEvent } from '../../../../contexts/course-subscriptions/domain/events/CourseSubscriptionWasDeleted';
+import { DecreaseSubscribedCoursesCounterOnCourseSubscriptionDeletedHandler } from '../../../../contexts/consumer-stats/application/events/DecreaseSubscribedCoursesCounterOnCourseSubscriptionDeleted';
+import { IncreaseSubscribedCoursesCounterOnCourseSubscriptionCreatedHandler } from '../../../../contexts/consumer-stats/application/events/IncreaseSubscribedCoursesCounterOnCourseSubscriptionCreated';
+import { CourseSubscriptionWasCompletedEvent } from '../../../../contexts/course-subscriptions/domain/events/CourseSubscriptionWasCompleted';
+import { IncreaseCompletedCoursesCounterOnCourseSubscriptionCompletedHandler } from '../../../../contexts/consumer-stats/application/events/IncreaseCompletedCoursesCounterOnCourseSubscriptionCompleted';
+import { CourseSubscriptionWasUncompletedEvent } from '../../../../contexts/course-subscriptions/domain/events/CourseSubscriptionWasUncompleted';
+import { DecreaseCompletedCoursesCounterOnCourseSubscriptionUncompletedHandler } from '../../../../contexts/consumer-stats/application/events/DecreaseCompletedCoursesCounterOnCourseSubscriptionUncompleted';
+
+const eventBus = DependencyContainer.get<EventBus>(InMemoryAsyncEventBus);
+
+const increaseSubscribedCoursesCounterOnCourseSubscriptionCreated =
+  DependencyContainer.get<
+    DomainEventSubscriber<CourseSubscriptionWasCreatedEvent>
+  >(IncreaseSubscribedCoursesCounterOnCourseSubscriptionCreatedHandler);
+
+const decreaseSubscribedCoursesCounterOnCourseSubscriptionDeleted =
+  DependencyContainer.get<
+    DomainEventSubscriber<CourseSubscriptionWasDeletedEvent>
+  >(DecreaseSubscribedCoursesCounterOnCourseSubscriptionDeletedHandler);
+
+const increaseCompletedCoursesCounterOnCourseSubscriptionCompleted =
+  DependencyContainer.get<
+    DomainEventSubscriber<CourseSubscriptionWasCompletedEvent>
+  >(IncreaseCompletedCoursesCounterOnCourseSubscriptionCompletedHandler);
+
+const decreaseCompletedCoursesCounterOnCourseSubscriptionDeleted =
+  DependencyContainer.get<
+    DomainEventSubscriber<CourseSubscriptionWasUncompletedEvent>
+  >(DecreaseCompletedCoursesCounterOnCourseSubscriptionUncompletedHandler);
+
+eventBus.addSubscribers([
+  increaseSubscribedCoursesCounterOnCourseSubscriptionCreated,
+  decreaseSubscribedCoursesCounterOnCourseSubscriptionDeleted,
+  increaseCompletedCoursesCounterOnCourseSubscriptionCompleted,
+  decreaseCompletedCoursesCounterOnCourseSubscriptionDeleted,
+]);
