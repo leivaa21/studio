d06b4f926b67f2b10e9f5207f879c0b584464cc8
Leivaa <adrianleiva@genially.com>
2023-08-27 20:39:27 +0200

    feat: handler

---
+++ apis/courses/src/contexts/course-stats/application/events/CreateCourseStatsOnCourseCreated.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@studio/dependency-injection';
+import { DomainEventClass } from '../../../shared/domain/DomainEvent';
+import { EventHandler } from '../../../shared/application/EventHandler';
+import { CourseId } from '../../../courses/domain/CourseId';
+import { MongoCourseStatsRepository } from '../../infrastructure/persistance/mongo/MongoCourseStatsRepository';
+import { CourseStatsRepository } from '../../domain/CourseStatsRepository';
+import { CourseWasCreatedEvent } from '../../../courses/domain/events/CourseWasCreated';
+import { CourseStats } from '../../domain/CourseStats';
+
+@Injectable({
+  dependencies: [MongoCourseStatsRepository],
+})
+export class CreateCourseStatsOnCourseCreatedHandler extends EventHandler<CourseWasCreatedEvent> {
+  public constructor(
+    private readonly courseStatsRepository: CourseStatsRepository
+  ) {
+    super();
+  }
+  subscribedTo(): DomainEventClass[] {
+    return [CourseWasCreatedEvent];
+  }
+  async on(domainEvent: CourseWasCreatedEvent): Promise<void> {
+    const courseId = CourseId.of(domainEvent.aggregateId);
+
+    const courseStats = CourseStats.new({ courseId });
+
+    await this.courseStatsRepository.create(courseStats);
+  }
+}
