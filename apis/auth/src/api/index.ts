a8a18097e584eb7e2c05b6d462e0a2f154d03f2a
Leivaa <adrianleiva@genially.com>
2023-10-13 20:59:53 +0200

    feat: relocated ports

--- apis/auth/src/api/index.ts
+++ apis/auth/src/api/index.ts
@@ -3,14 +3,13 @@ import { info } from '@studio/api-utils';
 import { app } from './app';
 import { configMongoose, connectToMongo } from './mongo/connection';
 import { options, uri } from './mongo/config';
+import { env } from './config/env';
 
 configMongoose();
 connectToMongo({ uri, options });
 
-const port = 5000;
-
 const { NODE_ENV } = process.env;
 
-app.listen(port, () => {
-  info(`Hey, running on http://localhost:${port} on env: ${NODE_ENV}`);
+app.listen(env.auth.port, () => {
+  info(`Hey, running on http://localhost:${env.auth.port} on env: ${NODE_ENV}`);
 });
