41a71f7fc2c6eafe7f0125cbb19dc1f8d49e7b5c
Leivaa <adrianleiva@genially.com>
2023-10-15 18:28:59 +0200

    feat: using InMemory EventBus on e2e tests

--- apis/courses/src/api/config/env.ts
+++ apis/courses/src/api/config/env.ts
@@ -14,6 +14,7 @@ export const env = {
   courses: {
     port: getEnvVariableOrDefault<number>('PORT', 5002),
     url: getEnvVariableOrDefault<string>('URL', 'http://localhost'),
+    mode: getEnvVariableOrDefault<string>('NODE_ENV', 'dev'),
   },
   mongo: {
     host: getEnvVariableOrDefault<string>('MONGO_HOST', 'localhost'),
