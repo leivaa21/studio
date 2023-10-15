ed62978b51d78233931718f05e6097bc5bfb8a5a
Leivaa <adrianleiva@genially.com>
2023-10-15 15:34:52 +0200

    feat: registered MongoLessonRepository as implementation

--- apis/courses/src/api/di/infrastructure.ts
+++ apis/courses/src/api/di/infrastructure.ts
@@ -3,6 +3,9 @@ import { MongoCourseRepository } from '../../contexts/courses/infrastructure/per
 import { CourseRepository } from '../../contexts/courses/domain/CourseRepository';
 import { CourseSchemaFactory } from '../../contexts/courses/infrastructure/persistance/mongo/CourseSchemaFactory';
 import { MongoCriteriaConverter } from '../../contexts/shared/infrastructure/mongo/MongoCriteriaConverter';
+import { LessonRepository } from '../../contexts/lessons/domain/LessonRepository';
+import { MongoLessonRepository } from '../../contexts/lessons/infrastructure/persistance/mongo/MongoLessonRepository';
+import { LessonSchemaFactory } from '../../contexts/lessons/infrastructure/persistance/mongo/LessonSchemaFactory';
 
 const courseSchemaFactory = new CourseSchemaFactory();
 const criteriaConverter = new MongoCriteriaConverter();
@@ -13,4 +16,11 @@ DependencyContainer.registerImplementation({
     courseSchemaFactory,
     criteriaConverter
   ),
+});
+
+const lessonSchemaFactory = new LessonSchemaFactory();
+
+DependencyContainer.registerImplementation({
+  constructor: LessonRepository,
+  implementation: new MongoLessonRepository(lessonSchemaFactory),
 });
